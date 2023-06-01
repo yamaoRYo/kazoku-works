938cafadcb64dab59cff9863b79902ce07520327
yamaoRYo <r.yamaguchi.g@gmail.com>
2023-06-01 09:39:13 +0900

    login_session

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   root "tops#index"
 
   resources :users, only: %i[new create]
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
