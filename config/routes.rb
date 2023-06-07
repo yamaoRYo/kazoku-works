09ad51442ceee18d95a429ea6fa8910bf330b8bf
yamaoRYo <r.yamaguchi.g@gmail.com>
2023-06-08 08:55:03 +0900

    家族ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "tops#index"
+  root "families#index"
 
   resources :users, only: %i[new create]
   get 'login' => 'user_sessions#new', :as => :login
