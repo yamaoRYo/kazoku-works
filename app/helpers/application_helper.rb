6a1a8700a0c7a3298fa06ef1c5f03111d5c13772
yamaoRYo <r.yamaguchi.g@gmail.com>
2023-06-10 13:40:49 +0900

    フラッシュの表示と画面サイズの調整_#33

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def render_turbo_stream_flash_messages
+    turbo_stream.prepend "flash", partial: "layouts/flash"
+  end
+
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when "success"
+      "success"  # green
+    when "error"
+      "danger"   # red
+    when "alert"
+      "warning"  # yellow
+    when "notice"
+      "info"     # blue
+    else
+      flash_type.to_s
+    end
+  end
 end
