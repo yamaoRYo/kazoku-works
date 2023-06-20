e6fd1209300da95bac63b9554e0422f4e61b23b4
yamaoRYo <r.yamaguchi.g@gmail.com>
2023-06-20 11:49:02 +0900

    Add:Userへカラムを追加し詳細ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
+  before_action :set_user, only: %i[show edit update]
 
   def new
     @user = User.new
@@ -15,9 +16,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @family = @user.family
+  end
+
+  def edit;
+  end
+
+  def update
+    if @user.update!(detail_params)
+      redirect_to @user, flash[:notice] = "ユーザー情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def detail_params
+    params.require(:user).permit(:relationship, :birthdate, :age, :constellation, :blood_type, :image)
   end
 end
