6f5abf65a3c3bbfdaa8b2881753dfed43a04fa8e
yamaoRYo <r.yamaguchi.g@gmail.com>
2023-07-08 15:38:44 +0900

    Add:招待機能の実装_#32

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,6 +1,11 @@
 class Family < ApplicationRecord
-    has_many :users
-    belongs_to :admin, class_name: 'User'
+  has_many :users
+  has_many :invitations
+  belongs_to :admin, class_name: 'User'
 
-    validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
+
+  def invite_user
+    invitations.create!
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_15_063310) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_062832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_15_063310) do
     t.date "established_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.string "token"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_id"], name: "index_invitations_on_family_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_15_063310) do
     t.index ["family_id"], name: "index_users_on_family_id"
   end
 
+  add_foreign_key "invitations", "families"
   add_foreign_key "users", "families"
 end
